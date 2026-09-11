8307121b692209740cacefdeae90743f6c436f1b
AgentSMDJ <sandermae036@gmail.com>
2026-09-11 11:30:18 +0200

    added an object containing the name imgs, and finded the section of which the apartments should be

---
+++ script.js
@@ -0,0 +1,13 @@
+const imgs = [
+    {img: "andalucia.webp"},
+    {img: "ecuador.webp"},
+    {img: "greece.jpg"},
+    {img: "norway.webp"},
+    {img: "pigraItaly.jpg"},
+    {img: "rome.webp"},
+    {img: "santorini.webp"},
+    {img: "spain.jpg"}
+]
+
+const apartments = document.querySelector("apartments");
+console.log(apartments)
