68cb188a8d0ece88dbb041f39b09491d6fbf438c
AgentSMDJ <sandermae036@gmail.com>
2026-09-14 09:05:21 +0200

    added js to make imgs inside the div and added div to eachs section

--- script.js
+++ script.js
@@ -1,23 +1,44 @@
 
 // imgs and their source for js create elements purposes
-const imgs = [
-    {img: "andalucia.webp"},
-    {img: "ecuador.webp"},
-    {img: "greece.jpg"},
-    {img: "norway.webp"},
-    {img: "pigraItaly.jpg"},
-    {img: "rome.webp"},
-    {img: "santorini.webp"},
-    {img: "spain.jpg"}
+const imgsList = [
+    {imgsrc: "andalucia.webp"},
+    {imgsrc: "ecuador.webp"},
+    {imgsrc: "greece.jpg"},
+    {imgsrc: "norway.webp"},
+    {imgsrc: "pigraItaly.jpg"},
+    {imgsrc: "rome.webp"},
+    {imgsrc: "santorini.webp"},
+    {imgsrc: "spain.jpg"}
 ]
 
+
+// get the apartments section
 const apartments = document.querySelector("#apartments");
 console.log(apartments)
 
-imgs.forEach(function(img){
+imgsList.forEach(function(imgs){
+
+    // make section
     let section = document.createElement("section");
 
+    // give the section the class apartment 
     section.classList.add("apartment");
 
+        // inside that section make an div
+        let div = document.createElement("div");
+
+            // inside the div make an img with src equial to the object
+            let img = document.createElement("img");
+
+                // inside img the source should be equial to the object currently on
+                img.setAttribute("src", "img/" + imgs.imgsrc);
+
+            // append the img inside the div
+            div.append(img)
+            
+        // append div insid the apartment
+        section.append(div);
+
+    // append the section inside apartments
     apartments.append(section);
 });
