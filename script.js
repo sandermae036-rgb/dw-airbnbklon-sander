93b5c27592ce397a072e1f839a4ba884c304dfe0
AgentSMDJ <sandermae036@gmail.com>
2026-09-15 09:07:26 +0200

    made the more into anchors for the other site to get id

--- script.js
+++ script.js
@@ -1,18 +1,15 @@
-
-console.log("started")
-
+console.log("script started");
 const url = "data/destinations.json";
 
 // fetch the json file
 async function fetchDestination(url, newTry) {
-    console.log(newTry);
 
     try {
         const firstResponse = await fetch(url);
         console.log(firstResponse);
 
         const data = await firstResponse.json();
-        console.log(data.destinations);
+        console.log(data);
 
         placeInHTML(data.destinations)
 
@@ -21,19 +18,26 @@ async function fetchDestination(url, newTry) {
     }
 }
 
+
+
+
+
+
 fetchDestination(url)
 
 // making the html ------------------------------------------------------------------------------------------
 // get the apartments section
 
 const apartments = document.querySelector("#apartments");
-console.log(apartments)
+console.log(apartments);
 
-function placeInHTML(apartmentData) {
 
-    apartmentData.forEach(function (apartment) {
+function placeInHTML(apartmentList) {
+
+    apartmentList.forEach(function (apartment) {
         // make section
         let section = document.createElement("section");
+        console.log(section);
 
         // give the section the class apartment 
         section.classList.add("apartment");
@@ -44,6 +48,7 @@ function placeInHTML(apartmentData) {
         // imgs ---------------------------------------------------------------
         // inside that section make an div
         let div = document.createElement("div");
+        console.log(div);
 
         // inside the div make an img with src equial to the object
         let img = document.createElement("img");
@@ -55,21 +60,27 @@ function placeInHTML(apartmentData) {
         img.setAttribute("alt", "Apartment");
 
         // append the img inside the div
-        div.append(img)
+        div.append(img);
 
         // append div insid the apartment
         section.append(div);
 
 
-        // paragraph -------------------------------------------------
-        // make a paragraph
-        let paragraph = document.createElement("p");
+        // anchor -------------------------------------------------
+        // make a anchor tag
+        let anchor = document.createElement("a");
 
         // text content = more
-        paragraph.textContent = "More"
+        anchor.textContent = "More";
 
+        // set attribute to the href of the detail site
+        anchor.setAttribute("href", "detail.html" + "?id=" + apartment.id);
+
+        // make it target blank because its better
+        anchor.setAttribute("target", "_blank");
         // append the paragraph with the txt 
-        section.append(paragraph);
+        section.append(anchor);
+
 
         // append the section inside apartments
         apartments.append(section);
@@ -77,3 +88,10 @@ function placeInHTML(apartmentData) {
     });
 
 }
+
+
+// function showDetail () {
+//     console.log(this.id)
+
+//     window.location = new URL();
+// };
