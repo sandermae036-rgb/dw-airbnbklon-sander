994d19be14a0509250457101a810799bee39759f
AgentSMDJ <sandermae036@gmail.com>
2026-09-15 08:04:37 +0200

    remade the js with the data from fetching it, instead of writting the data myself

--- htmlMaker.js
+++ htmlMaker.js
@@ -1,45 +1,62 @@
 
-// imgs and their source for js create elements purposes
-const imgsList = [
-    {imgsrc: "andalucia.webp"},
-    {imgsrc: "ecuador.webp"},
-    {imgsrc: "greece.jpg"},
-    {imgsrc: "norway.webp"},
-    {imgsrc: "pigraItaly.jpg"},
-    {imgsrc: "rome.webp"},
-    {imgsrc: "santorini.webp"},
-    {imgsrc: "spain.jpg"}
-]
+console.log("started")
 
+const url = "data/destinations.json";
 
+// fetch the json file
+async function fetchDestination(url, newTry) {
+    console.log(newTry);
+
+    try {
+        const firstResponse = await fetch(url);
+        console.log(firstResponse);
+
+        const data = await firstResponse.json();
+        console.log(data.destinations);
+
+        placeInHTML(data.destinations)
+
+    } catch (error) {
+
+    }
+}
+
+fetchDestination(url)
+
+// making the html ------------------------------------------------------------------------------------------
 // get the apartments section
+
 const apartments = document.querySelector("#apartments");
 console.log(apartments)
 
-imgsList.forEach(function(imgs){
+function placeInHTML(apartmentData) {
 
-    // make section
-    let section = document.createElement("section");
+    apartmentData.forEach(function (apartment) {
+        // make section
+        let section = document.createElement("section");
 
-    // give the section the class apartment 
-    section.classList.add("apartment");
+        // give the section the class apartment 
+        section.classList.add("apartment");
+
+        // give the section the id from the destination 
+        section.id = apartment.id;
 
         // imgs ---------------------------------------------------------------
         // inside that section make an div
         let div = document.createElement("div");
 
-            // inside the div make an img with src equial to the object
-            let img = document.createElement("img");
+        // inside the div make an img with src equial to the object
+        let img = document.createElement("img");
 
-                // inside img the source should be equial to the object currently on
-                img.setAttribute("src", "img/" + imgs.imgsrc);
+        // inside img the source should be equial to the object currently on
+        img.setAttribute("src", "img/" + apartment.image);
 
-                // make alt alt
-                img.setAttribute("alt", "Apartment");
+        // make alt alt
+        img.setAttribute("alt", "Apartment");
 
-            // append the img inside the div
-            div.append(img)
-            
+        // append the img inside the div
+        div.append(img)
+
         // append div insid the apartment
         section.append(div);
 
@@ -48,12 +65,15 @@ imgsList.forEach(function(imgs){
         // make a paragraph
         let paragraph = document.createElement("p");
 
-            // text content = more
-            paragraph.textContent = "More" 
+        // text content = more
+        paragraph.textContent = "More"
 
         // append the paragraph with the txt 
         section.append(paragraph);
 
-    // append the section inside apartments
-    apartments.append(section);
-});
+        // append the section inside apartments
+        apartments.append(section);
+
+    });
+
+}
